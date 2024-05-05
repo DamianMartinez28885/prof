b7e1650760ffc2f83fbef2d1622a4d58e96fc027
Damian Martinez <dmartinez885@alumnos.frh.utn.edu.ar>
2024-05-04 23:10:48 -0300

    actualizar js

---
+++ Jscript/js01_sentencias/sentencias.js
@@ -0,0 +1,4 @@
+alert("mensaje a escribir-->Funciones de css y CSS funciona como complemento a la información que forma parte de un sitio web. Mientras que el código en HTML incluye todos los datos, el código en CSS se encarga de darles formato y presentarlos visualmente a través de un navegador ")
+document.write("<header><h1>Funciones de css</h1></header>");
+document.write("CSS funciona como complemento a la información que forma parte de un sitio web. Mientras que el código en HTML incluye todos los datos, el código en CSS se encarga de darles formato y presentarlos visualmente a través de un navegador")
+;
