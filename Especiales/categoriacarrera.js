ce31bd86b4c7e387817a76bcd6d6ce7dd4178ce1
Damian Martinez <dmartinez885@alumnos.frh.utn.edu.ar>
2024-05-23 23:12:52 -0300

    act

---
+++ Especiales/categoriacarrera.js
@@ -0,0 +1,12 @@
+var objetoJson = `{
+	"varCategoria":[
+		{"value":"mini","Descripcion":"Categoria MINI"},
+		{"value":"25","Descripcion":"Categoria 18-25"},
+		{"value":"30","Descripcion":"Categoria 26-30"},
+		{"value":"35","Descripcion":"Categoria 31-35"},
+		{"value":"40","Descripcion":"Categoria 36-40"},
+        {"value":"45","Descripcion":"Categoria 41-45"},
+        {"value":"55","Descripcion":"Categoria 46-55"},
+        {"value":"Mayor","Descripcion":"Categoria MAYOR"}
+	]
+}`;
