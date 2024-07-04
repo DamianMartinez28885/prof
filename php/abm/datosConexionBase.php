b208927e5d2adfa4bdac65e48c5151923389ab0f
Damian Martinez <dmartinez885@alumnos.frh.utn.edu.ar>
2024-07-04 14:21:22 -0300

    act

--- php/abm/datosConexionBase.php
+++ php/abm/datosConexionBase.php
@@ -1,15 +1,18 @@
+
+<?php
+$dbname="bvkbecdbl2l2s8fbc9h1";
+$host="bvkbecdbl2l2s8fbc9h1-mysql.services.clever-cloud.com";
+$user ="uctwb0vvhztukj2a";
+$password = "oVFYQ36gVsQp9jJ0q4kz";
+
+
+/* Base de datos del west
 <?php
 $dbname="b3c1dnzjfajzfextbbcs";
 $host="localhost";
 $user ="id22247569_damianmartinez";
 $password = "Basedatos1.";
-
-
-/*
-define("SERVER","b3c1dnzjfajzfextbbcs-mysql.services.clever-cloud.com:3306");
-define("USUARIO","ubtjuyglm1htsjy0");
-define("PASS","yzK5Hjq3yudhhhfTdyJ2");
-define("BASE","b3c1dnzjfajzfextbbcs");
+?>
 */
 
 ?>
